50acc55ceb00f88e61815374cc215544a2805124
mradib <mr.adib@yandex.com>
2019-04-20 18:31:27 +0600

    line delete

---
+++ Class5.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<form action="#" method="post">
+Name: <input type="text" name="name"><br>
+<input type="submit" name ="add">
+</form>
+<?php 
+if(isset($_POST['add']){
+	$name = $_POST['submit'];
+	
+}
+?>  
+</body>
+</html>
